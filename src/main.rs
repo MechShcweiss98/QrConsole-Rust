79a4081199991aa70ed096ab582d876c773cb67f
drrkermazyxokv <drrkermazyxokv@gmail.com>
2025-01-14 17:25:39 +0000

    reforms in main rs, implement som validations and modules

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
-use image::Luma;
 use qrcode::QrCode;
+
+fn generate_qr_code (data:  &str) -> Result<QrCode, String>{
+    QrCode::new(data).map_err(|e|format!("Error to generate a QR code: {}", e))
+}
+
+fn render_qr_code_to_string(code:&QrCode) ->String {
+    code.render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2,1)
+        .build()
+}
 
 fn main() {
     let ssid = "NAME-OF.RED";
     let encryption = "SECURYTT"; //Example WPA2
     let password = "NETWORK-PASSWORD"; 
 
+    if ssid.is_empty() || pasasword.is_empty(){
+        eprintln!("SSID and password are required");
+        return;
+    }
+
+
     let wifi_info = format!("WIFI:S:{};T:{};P:{};;", ssid, encryption, password);
 
-    let code = QrCode::new(wifi_info).expect("Cannot generate the QR code");
+    let code = match generate_qr_code(&wifi_info){
+        Ok(code) => code,
+        Err(err) => {
+            eprintln!("{}", err);
+            return;
+        }
+    }
 
-    let string = code
-        .render::<char>()
-        .quiet_zone(false)
-        .module_dimensions(2, 1)
-        .build();
-
-    print!("{}", string);
+    let qr_string = render_qr_code_to_string(&code);
+    print!("{}", qr_string);
 }
