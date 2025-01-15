97b1c06348b2e761cb9c770a2e97f77692be4767
darmarzz <123565631+DMarzzucco@users.noreply.github.com>
2025-01-15 20:25:21 -0300

    implemente un input para tener que escribir los datos desde seguimientos en la consola, en vez de tener que acceder al codigo fuente para hacer lo reiterados cambios

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 mod qr_code;
 mod render;
 mod wifi;
+mod utils;
 
 use qr_code::generate_qr_code;
 use render::{QrRenderer, TextQrRenderer};
+use utils::input::prompt_user;
 use wifi::builder::WifiInfoBuilder;
 
 fn main() {
-    let ssid = "NAME-OF.RED";
-    let encryption = "SECURITY"; //Example WPA2
-    let password = "NETWORK-PASSWORD";
+    let ssid = prompt_user("Enter the network name (SSID):");
+    let encryption = prompt_user("Enter the security system (WEP/WPA/WPA2):");
+    let password = prompt_user("Enter the network password:");
 
     let wifi_info = match WifiInfoBuilder::new()
-        .ssid(ssid)
-        .encryption(encryption)
-        .password(password)
+        .ssid(&ssid)
+        .encryption(&encryption)
+        .password(&password)
         .build()
     {
         Ok(info) => info,
